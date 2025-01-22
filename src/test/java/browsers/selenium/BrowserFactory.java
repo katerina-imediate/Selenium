80543e97bd26ce5b1e269bb89d93fc3e0ac99fd7
katerina-imediate <katexrg@yahoo.com>
2025-01-22 14:07:28 +0200

    add options

--- src/test/java/browsers/selenium/BrowserFactory.java
+++ src/test/java/browsers/selenium/BrowserFactory.java
@@ -3,6 +3,7 @@ package browsers.selenium;
 import browsers.enums.Browsers;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
@@ -10,6 +11,8 @@ public class BrowserFactory {
 
     public static WebDriver launch(Browsers browser) {
         if (browser.equals(Browsers.CHROME)) {
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("start-maximized");
             return new ChromeDriver();
         } else if (browser.equals(Browsers.FIREFOX)) {
             return new FirefoxDriver();
