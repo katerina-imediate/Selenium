1e2a4004c88b420773f13448e6c5e4cd0ae6de67
katerina-imediate <katexrg@yahoo.com>
2025-01-27 19:23:44 +0200

    rm static launch

--- src/test/java/sauce_tests/tests/testng/testcases/Webdriver.java
+++ src/test/java/sauce_tests/tests/testng/testcases/Webdriver.java
@@ -2,35 +2,30 @@ package sauce_tests.tests.testng.testcases;
 
 import browsers.enums.Browsers;
 import browsers.selenium.BrowserFactory;
-import browsers.enums.Browsers;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.chromium.ChromiumDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.*;
 import sauce_tests.config.Waits;
-
 import java.time.Duration;
-//import sauce_tests.config.Waits;
-//import java.time.Duration;
+
 
 public abstract class Webdriver {
 
-    protected WebDriver driver;
-    private Browsers browser = Browsers.CHROME;
+    WebDriver driver = new ChromeDriver();
+//    private Browsers browser = Browsers.CHROME;
     private String url = "https://www.saucedemo.com/";
 
     @BeforeMethod
     public void setup() {
-        driver = BrowserFactory.launch(browser);
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(Waits.WAIT_IMPLICIT));
-        driver.manage().window().maximize();
+//        driver = BrowserFactory.launch(driver);
+//        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(Waits.WAIT_IMPLICIT));
+//        driver.manage().window().maximize();
         driver.get(url);
     }
 
-    @AfterMethod
-    public void tearDown() {
-       driver.quit();
-    }
+//    @AfterMethod
+//    public void tearDown() {
+//       driver.quit();
+//    }
 }
