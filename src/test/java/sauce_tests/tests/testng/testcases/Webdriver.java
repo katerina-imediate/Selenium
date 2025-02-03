4f33d6d51a08e1ec624bd19cb5d84fb9d7966865
katerina-imediate <katexrg@yahoo.com>
2025-02-03 09:10:54 +0200

    rut

--- src/test/java/sauce_tests/tests/testng/testcases/Webdriver.java
+++ src/test/java/sauce_tests/tests/testng/testcases/Webdriver.java
@@ -5,9 +5,15 @@ import browsers.selenium.BrowserFactory;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.chromium.ChromiumDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.*;
 import sauce_tests.config.Waits;
+
+import java.io.File;
+import java.io.IOException;
 import java.time.Duration;
 
 
@@ -15,11 +21,14 @@ public abstract class Webdriver {
 
 
 
-       ChromeDriver driver = new ChromeDriver();
+    protected   ChromeDriver driver;
+    private static ChromeDriverService service;
+
 
 //    private Browsers browser = Browsers.CHROME;
 
         String url = "https://www.saucedemo.com/";
+
 
     @BeforeClass
     public void setup() {
